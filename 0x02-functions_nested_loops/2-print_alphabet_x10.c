ce29af9a813b02d1bcd3b2271b980dd491bda80c
FortuneMal <fmalaza512@gmail.com>
2023-10-11 13:04:13 -0700

    2 prints

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,17 +2,19 @@
 
 /**
  * print_alphabet_x10 - Print lowercase alphabet 10 times
+ *
+ * Return: 0
  */
 
 void print_alphabet_x10(void)
 {
-	char num, letter;
+	char c, i;
 
-	for (num = 0; num <= 9; num++)
+	for (c = 0; i <= 9; num++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
+		for (c = 'a'; c  <= 'z'; c++)
 		{
-			_putchar(letter);
+			_putchar(c);
 		}
 		_putchar('\n');
 	}
