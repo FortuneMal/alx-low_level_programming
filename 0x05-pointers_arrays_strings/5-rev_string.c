d468bf408f91edb139de06fad9299d2ebdd019d0
FortuneMal <fmalaza512@gmail.com>
2023-10-16 11:30:58 -0700

    5 pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,7 @@
  */
 void rev_string(char *s)
 {
-	char rev = s[0]
+	char rev = s[0];
 	int counter = 0;
 	int i;
 
@@ -16,7 +16,7 @@ void rev_string(char *s)
 	for (i = 0; i < counter; i++)
 	{
 		counter--;
-		rev = s{i};
+		rev = s[i];
 		s[i] = s[counter];
 		s[counter] = rev;
 	}
